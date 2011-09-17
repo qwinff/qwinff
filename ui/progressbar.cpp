0dca34b491a3b63ff3490319add763d83aaf4fd9
Timothy Lin <timothy.lin1@gmail.com>
2011-09-17 13:47:05 +0800

    Extracted progressbar gradient colors to macros.

--- ui/progressbar.cpp
+++ ui/progressbar.cpp
@@ -2,6 +2,8 @@
 #include <QHBoxLayout>
 #include <QPainter>
 
+#define COLOR_MARGIN QColor(200,220,255)
+#define COLOR_CENTER QColor(150,200,220)
 
 ProgressBar::ProgressBar(QWidget *parent) :
     QWidget(parent), m_percentage(0)
@@ -26,9 +28,9 @@ void ProgressBar::paintEvent(QPaintEvent*)
             // draw progress bar
             QRect rect_progress(0, 0, (width()*m_percentage/100 - 1), height()-1);
             QLinearGradient gradient(0, 0, 0, rect_progress.bottom());
-            gradient.setColorAt(0, QColor(200,220,255));
-            gradient.setColorAt(0.5, QColor(150,200,220));
-            gradient.setColorAt(1, QColor(200,220,255));
+            gradient.setColorAt(0, COLOR_MARGIN);
+            gradient.setColorAt(0.5, COLOR_CENTER);
+            gradient.setColorAt(1, COLOR_MARGIN);
             painter.setBrush(gradient);
             painter.setPen(Qt::NoPen);  // Don't draw the border.
             painter.drawRect(rect_progress);
