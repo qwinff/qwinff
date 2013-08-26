a32b73a6264c50748d32a4bc087c66bcaece8666
Timothy Lin <lzh9102@gmail.com>
2013-08-26 18:00:40 +0800

    Call activateWindow() when all tasks have finished.

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -132,6 +132,7 @@ void MainWindow::task_finished(int /*exitcode*/)
 void MainWindow::all_tasks_finished()
 {
     Notification::send("QWinFF", tr("All tasks has finished."), NotifyLevel::INFO);
+    activateWindow(); // notify the user (make taskbar entry blink)
     refresh_action_states();
 
     if (PowerManagement::implemented() && m_poweroff_button->isChecked()) {
