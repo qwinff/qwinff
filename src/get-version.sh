067b9cef50f2705cacf1516c9e5dcf25353cd2cc
Timothy Lin <lzh9102@gmail.com>
2013-08-28 20:33:59 +0800

    Update get-version.sh comments.

--- src/get-version.sh
+++ src/get-version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# print the version string in version.h
+# print the human-readable version string in version.h
 
 cd "`dirname $0`"
 sed -n 's/#define VERSION_STRING "\([^"]*\)"/\1/p' version.h
