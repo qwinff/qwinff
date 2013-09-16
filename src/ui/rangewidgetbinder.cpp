0cae8e8bfe4d398ef6bd0fe7810b04394abe310a
Timothy Lin <lzh9102@gmail.com>
2013-09-16 16:31:34 +0800

    backout changeset c3e9a33b7643.

--- src/ui/rangewidgetbinder.cpp
+++ src/ui/rangewidgetbinder.cpp
@@ -16,8 +16,13 @@ void RangeWidgetBinder::sync_sel_to_edit()
 {
     int begin_time = m_selector->beginValue();
     int end_time = m_selector->endValue();
+    bool from_begin = (begin_time == 0);
+    bool to_end = (end_time == m_selector->maxValue());
+
     m_rangeEdit->setBeginTime(begin_time);
     m_rangeEdit->setEndTime(end_time);
+    m_rangeEdit->setFromBegin(from_begin);
+    m_rangeEdit->setToEnd(to_end);
 }
 
 // sync m_rangeEdit (text) value to m_selector (visual)
