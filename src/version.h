ccc002c6f4ff7a5b1e7e2f22d0a82bbe66e39922
Che-Huai Lin <lzh9102@gmail.com>
2014-02-09 21:46:05 -0500

    Bump version to 0.2.0

--- src/version.h
+++ src/version.h
@@ -19,10 +19,10 @@
 #define VERSION_H
 
 // human-readable version string (VersionName)
-#define VERSION_STRING "0.1.9"
+#define VERSION_STRING "0.2.0"
 
 // machine-readable version id (VersionId)
 // the update-checker compares versions using this integer
-#define VERSION_INTEGER 19
+#define VERSION_INTEGER 20
 
 #endif // VERSION_H
